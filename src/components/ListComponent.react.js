ffa13088cee60edc78422857bea35c8e7079dc48
garam <studio@gar.am>
2016-05-28 16:56:14 +0900

    Convert timestamp to general time.

--- src/components/ListComponent.react.js
+++ src/components/ListComponent.react.js
@@ -6,10 +6,31 @@ export default class ListComponent extends React.Component {
   }
 
   render() {
+
+    let today = new Date(this.props.time);
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //  1월은 0으로나옴
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+
+    if (dd < 10) {
+      dd = `0${dd}`;
+    }
+    if (mm < 10) {
+      mm = `0${mm}`;
+    }
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    today = `${mm}-${dd} ${hours}:${minutes}`;
+
     return (
       <tr>
-        <td>1</td>
-        <td>2016-05-12 14:00</td>
+        <td>{this.props.order}</td>
+        <td>{today}</td>
         <td><button>Remove</button></td>
       </tr>
     );
