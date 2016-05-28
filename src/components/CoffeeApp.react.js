2868efd282fb4d91ecda11618dbf172eafebf45c
garam <studio@gar.am>
2016-05-28 10:51:32 +0900

    First Commit.

---
+++ src/components/CoffeeApp.react.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class KanbanApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>화이팅</h1>
+      </div>
+    );
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CoffeeApp from './components/CoffeeApp.react.js';
+
+const App = () => <CoffeeApp />;
+
+ReactDOM.render(<App />, document.getElementById('app'));
